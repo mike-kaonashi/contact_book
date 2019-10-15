28e439715be0681c1c4dffc38defbc84eab22ace
Minh Phan <minhpq.contact@gmail>
2019-10-15 10:22:01 +0700

    Add custom error

---
+++ Assignment1/contactbook/errors/exceptions.py
@@ -0,0 +1,34 @@
+class NotFitSchemaError(Exception):
+    """The data doesn't fit with the pre-defined schema.
+    """
+    pass
+
+
+class NotAvailableFileError(Exception):
+    """File does not exist or can't be found.
+    """
+    pass
+
+
+class NotInJsonFormatError(Exception):
+    """File not in Json format for parsing stuffs
+    """
+    pass
+
+
+class NullFilterConditionError(Exception):
+    """Null params for filtering stuffs
+    """
+    pass
+
+
+class NotSupportedTypeError(Exception):
+    """Type not in our supported list
+    """
+    pass
+
+
+class NotAvailableValueError(Exception):
+    """Data constraints not include negative number or double/float
+    """
+    pass
